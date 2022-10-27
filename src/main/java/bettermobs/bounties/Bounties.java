97b9cf4232e6701e977e4a53f0e3cf1d200dab4c
Better Mobs <wikimal200310@gmail.com>
2022-10-27 23:50:57 +0200

    2

--- src/main/java/bettermobs/bounties/Bounties.java
+++ src/main/java/bettermobs/bounties/Bounties.java
@@ -1,37 +1,40 @@
 package bettermobs.bounties;
 
-import org.bukkit.command.CommandMap;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandSendEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Collection;
+import java.io.Reader;
 
 public final class Bounties extends JavaPlugin implements Listener {
 
-    String plugin_name="Bounties";
-    String command_prefix="bounties";
+PluginDescriptionFile plugin_desc = this.getDescription();
     @Override
     public void onEnable() {
-
         // Plugin startup logic
-        System.out.println("["+plugin_name+"] Started successfully!");
+        System.out.println("Started successfully!");
 
-        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        System.out.println("["+plugin_name+"] Stopped!");
+        System.out.println("Stopped!");
+
     }
 
-    @EventHandler
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        String commands_prefix =""+ChatColor.DARK_GREEN+ChatColor.BOLD+"Bounties"+ChatColor.GRAY+ChatColor.BOLD+" | ";
 
-    public void onCommand(PlayerCommandSendEvent event){
+        if(command.getName().equalsIgnoreCase("bounties") && sender instanceof Player){
+            sender.sendMessage(commands_prefix+"\n"+ChatColor.AQUA+ChatColor.BOLD+"Version: "+ChatColor.DARK_GREEN+plugin_desc.getVersion());
+        }
 
+        return true;
     }
 }
