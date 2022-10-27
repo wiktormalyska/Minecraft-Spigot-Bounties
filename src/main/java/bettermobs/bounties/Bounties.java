1e7ee6094ef11f4ea1a393467d2ae98939df45c1
Better Mobs <wikimal200310@gmail.com>
2022-10-27 19:00:37 +0200

    setup

---
+++ src/main/java/bettermobs/bounties/Bounties.java
@@ -0,0 +1,41 @@
+package bettermobs.bounties;
+
+import org.bukkit.command.CommandMap;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandSendEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collection;
+
+public final class Bounties extends JavaPlugin implements Listener {
+
+    String plugin_name="Bounties";
+    String command_prefix="bounties";
+    @Override
+    public void onEnable() {
+
+        // Plugin startup logic
+        System.out.println("["+plugin_name+"] Started successfully!");
+
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("["+plugin_name+"] Stopped!");
+    }
+
+    @EventHandler
+
+    public void onCommand(PlayerCommandSendEvent event){
+        Player player = event.getPlayer();
+        Collection<String> command = event.getCommands();
+        if (command.contains(command_prefix)){
+            player.sendMessage("Elo");
+        }
+    }
+}
---
+++ src/main/java/bettermobs/bounties/commands/bounty.java
@@ -0,0 +1,11 @@
+package bettermobs.bounties.commands;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class bounty implements Listener {
+    String permission = "";
+
+}
